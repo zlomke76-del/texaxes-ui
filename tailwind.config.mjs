03057befa94fa0d47e4e80f66539c09934d31e71
zlomke76-del <zlomke76@gmail.com>
2026-03-26 19:46:22 -0500

    Create tailwind.config.mjs

---
+++ tailwind.config.mjs
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
