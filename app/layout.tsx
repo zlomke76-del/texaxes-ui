aef30f027ddaeaaae54cdcd2eb6664ec1b9ca621
zlomke76-del <zlomke76@gmail.com>
2026-04-04 19:46:30 -0500

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,11 @@ import type { Metadata } from "next";
 export const metadata: Metadata = {
   title: "Tex Axes UI",
   description: "Tex Axes frontend",
+  icons: {
+    icon: "/images/texaxes-favicon.svg",
+    shortcut: "/images/texaxes-favicon.svg",
+    apple: "/images/texaxes-favicon.svg",
+  },
 };
 
 export default function RootLayout({
